35957aa51697733e9566d1a4bb94fa1bd73b1301
Sven Thiessen <sv.thiessen@gmail.com>
2017-04-20 12:14:11 +0200

    Set reference to new organisation name

--- storage/cloud.go
+++ storage/cloud.go
@@ -8,7 +8,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/service/s3"
-	"github.com/MapOnline/go-core/logger"
+	"github.com/MapcreatorEU/go-core/logger"
 )
 
 type CloudStorageConfiguration struct {
