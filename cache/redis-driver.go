26bdc34ebdd5d1dfd550ba51861aa75e4f4fed88
Sven Thiessen <sv.thiessen@gmail.com>
2017-04-20 12:12:07 +0200

    Add redis cache methods

---
+++ cache/redis-driver.go
@@ -0,0 +1,53 @@
+package cache
+
+import (
+	"github.com/go-redis/redis"
+	"time"
+)
+
+type RedisDriver struct {
+	client *redis.Client
+}
+
+func NewRedisDriver(Address string, Password string, DB int)(*RedisDriver) {
+	return &RedisDriver{
+		client: redis.NewClient(&redis.Options{
+			Addr: Address,
+			Password: Password,
+			DB: DB,
+		}),
+	}
+}
+
+func (Driver *RedisDriver) SetKeyWithDuration(Key string, Value string, expiration time.Duration) (error) {
+	_, err := Driver.client.Set(Key, Value, expiration).Result()
+	return err
+}
+
+func (Driver *RedisDriver) SetKey(Key string, Value string) (error) {
+	return Driver.SetKeyWithDuration(Key, Value, 0)
+}
+
+func (Driver *RedisDriver) HasKey(Key string) (bool, error) {
+	_, err := Driver.client.Get(Key).Result()
+
+	if err != nil {
+		return false, err
+	}
+
+	return true, nil
+}
+
+func (Driver *RedisDriver) SAdd(Key string, Value string) (error) {
+	_, err := Driver.client.SAdd(Key, Value).Result()
+	return err
+}
+
+func (Driver *RedisDriver) SRem(Key string, Value string) (error) {
+	_, err := Driver.client.SRem(Key, Value).Result()
+	return err
+}
+
+func (Driver *RedisDriver) SHasKey(Key string, Value string) (bool, error) {
+	return Driver.client.SIsMember(Key, Value).Result()
+}
